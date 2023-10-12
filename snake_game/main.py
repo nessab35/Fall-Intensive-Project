13534d82444f599873cc564dda504200e5bb1b91
nessab35 <nessa.begay@students.dominican.edu>
2023-10-12 11:40:11 -0700

    updated game

--- snake_game/main.py
+++ snake_game/main.py
@@ -4,10 +4,10 @@ from pygame.locals import * # from pygame.locals importing "K_UP", etc and "impo
 import time # importing time
 import random # importing random
 
-size = 40 # assigning size to 40 so the body of thee snake is 40 pizels apart
+size = 40 # assigning size to 40 so the body of the snake is 40 pizels apart
 pink = (238, 148, 199) # assinging pink to rgb color
 black = (0, 0, 0) # assigning black to rgb color
-white = (255, 255, 255) # assinging white to rgb color
+screen_size = (1000,800) # assinging screen size
 
 
 class Strawberry: #create strawberry class
@@ -28,7 +28,8 @@ class Strawberry: #create strawberry class
         self.y = random.randint(0, 19) * size # divide screen size y=800 by 40 and got 20 * size so doesn't go out the window restrict it 1 lower=19
 
 
-# making classt for the snake
+
+# making class for the snake
 class Snake:
     # initializing parent_screen bascially the screen and length
     def __init__(self, parent_screen, length):
@@ -45,6 +46,7 @@ class Snake:
         self.length += 1 # increase length by 1
         self.x.append(-1) # append x -1
         self.y.append(-1) # append y -1
+
     # def move_left
     def move_left(self):
         self.direction = 'left'
@@ -63,8 +65,8 @@ class Snake:
 
     # creating a function so the snake "walks"
     def walk(self):
-
-        for i in range (self.length-1,0, -1): # creating a loop so the blcoks after the snake head follows the snake head
+        # creating a loop so the blcoks after the snake head follows the snake head
+        for i in range (self.length-1,0, -1):
             self.x[i] = self.x[i-1]
             self.y[i] = self.y[i-1]
 
@@ -86,15 +88,27 @@ class Snake:
             self.parent_screen.blit(self.image, (self.x[i], self.y[i])) #drawing the block image at x,100 and y,100
         pygame.display.flip() # update entire screen with whatever is drawn on parent_screen
 
+
+
 # creating class for game
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000,800)) # assigning surface to a window 100 x 800 pixels
+        pygame.display.set_caption("Snake and Strawberry-Nessa Begay") # setting caption of window
+
+        pygame.mixer.init()
+        self.play_background_music() # playing background music
+        self.surface = pygame.display.set_mode((screen_size)) # assigning surface to a window 100 x 800 pixels
         self.snake = Snake(self.surface, 1) # assigning snake to the snake class then passed onto the surface, making the snake 6 block long
         self.snake.draw() # drawing the snake onto the surface (window)
         self.strawberry = Strawberry(self.surface) # adding strawberry on game screen
         self.strawberry.draw() # drawing strawberry on screen
+
+    # defining display_score
+    def display_score(self):
+        font = pygame.font.SysFont('timesnewroman', 30) # assinging font to certain font and size
+        score = font.render(f"Score: {self.snake.length}", True, (black)) # displaying score with color
+        self.surface.blit(score, (800,10)) # putting score on scren with position
 
     # defining collsion using x,y,1,2
     def is_collision(self, x1, y1, x2, y2):
@@ -104,11 +118,16 @@ class Game:
             
         return False # or return false
 
-    # defining display_score
-    def display_score(self):
-        font = pygame.font.SysFont('timesnewroman', 30)
-        score = font.render(f"Score: {self.snake.length}", True, (black))
-        self.surface.blit(score, (800,10))
+    # defining background music
+    def play_background_music(self):
+        pygame.mixer.music.load("resources/bg_music_.mp3")  # loading backgroud song
+        pygame.mixer.music.play() # playing the sound
+
+    # defining function for play sound for ding and crash
+    def play_sound(self, sound):
+        sound = pygame.mixer.Sound(f"resources/{sound}.mp3") # assigning sound to play certain soud
+        pygame.mixer.Sound.play(sound) # playing sound
+
     # defining play so all pieces go onto the screen
     def play(self):
         self.snake.walk()
@@ -116,25 +135,35 @@ class Game:
         self.display_score()
         pygame.display.flip()
 
-        # snake colliding with strawberry
-        if self.is_collision(self.snake.x[0], self.snake.y[0], self.strawberry.x, self.strawberry.y): 
+        # snake eating strawberry
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.strawberry.x, self.strawberry.y):
+            self.play_sound("eating")
             self.snake.increase_length() # increaseing length if there is a collision with snake and strawberry
             self.strawberry.move() # then move straberry
 
         # snake colliding with itself
         for i in range(3, self.snake.length):
             if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                self.play_sound("crash")
                 raise "Game over!"
-
+        
+        # snake goes out of bounds then game is over
+        if not (0 <= self.snake.x[0] <= 1000 and 0 <= self.snake.y[0] <= 800):
+            self.play_sound("crash")
+            raise "Game over!"
+        
     # defining game over
     def show_game_over(self):
         self.surface.fill(black)
         font = pygame.font.SysFont('timesnewroman', 30)
-        line_1 = font.render(f"Game Over! Your score is {self.snake.length}", True, (white))
+        line_1 = font.render(f"Game Over! Your score is: {self.snake.length}", True, (pink))
         self.surface.blit(line_1, (200,300))
-        line_2 = font.render("To play again, press ENTER. To exit press ESC!", True, (white))
+        line_2 = font.render("To play again, press ENTER. To exit press ESC!", True, (pink))
         self.surface.blit(line_2, (200, 350))
-        pygame.display.flip()
+
+        pygame.display.flip() # display flip is used to display onto full screen
+
+        pygame.mixer.music.pause()
 
     # defining reset
     def reset(self):
@@ -154,6 +183,7 @@ class Game:
                         running = False #game return false/ browser will be closed
 
                     if event.key == K_RETURN: # if enter/return is pushed
+                        pygame.mixer.music.unpause()
                         pause = False #then the game restarts
 
                     if not pause:
@@ -178,7 +208,7 @@ class Game:
                 pause = True
                 self.reset()
 
-            time.sleep(.25) # setting speed of snake to .25 seconds
+            time.sleep(.1) # setting speed of snake to .25 seconds
 
 
 if __name__ == "__main__":
